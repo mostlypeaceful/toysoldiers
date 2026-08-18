670160ba91e561a148b5d706ece538badbbc1740
basedpotato <153902712+mostlypeaceful@users.noreply.github.com>
2026-08-17 17:36:03 -0700

    Delete TS2/branches/TS2_SteamV2/src directory
    cleaning

--- TS2/branches/TS2_SteamV2/src/GameApp/post-build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-
-set DST=%1
-
-copy /y "%SigEngine%\Src\External\Steamworks SDK 1.25\redistributable_bin\steam_api.dll" %DST%
-copy /y "%SigEngine%\Src\External\Theora\lib\*.dll" %DST%
-
-endlocal
